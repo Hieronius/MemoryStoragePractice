08be3aef09e11e95f887eb6c9b8ff3a5b22edf60
Arsenty Halimovsky <ghost12@yandex.ru>
2023-04-30 20:04:40 +0300

    Add table view cell

--- MemoryStoragePractice/MemoryStoragePractice/TableViewController.swift
+++ MemoryStoragePractice/MemoryStoragePractice/TableViewController.swift
@@ -9,7 +9,25 @@ import UIKit
 
 final class TableViewController: UITableViewController {
     
+    
+    
+    @IBOutlet weak var cellTitle: UILabel!
+    @IBOutlet weak var cellDetail: UILabel!
+    
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        
+    }
+    
 }
---
+++ MemoryStoragePractice/MemoryStoragePractice/UserTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  tableViewCell.swift
+//  MemoryStoragePractice
+//
+//  Created by Арсентий Халимовский on 30.04.2023.
+//
+
+import UIKit
+
+final class UserTableViewCell: UITableViewCell {
+    
+    
+}
